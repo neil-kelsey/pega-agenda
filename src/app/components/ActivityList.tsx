1f2bbbc01659ee0d611f3a68d5c44415c3e16791
Neil Kelsey <neil.kelsey@gmail.com>
2024-08-08 17:06:02 +0100

    feat: Fixing layout bug from introducing a div for the key at top level - replaced with fragment

--- src/app/components/ActivityList.tsx
+++ src/app/components/ActivityList.tsx
@@ -43,37 +43,37 @@ const ActivityList: React.FC<ActivityListProps> = ({ selectedDate, timeDifferenc
         const activityLength = activity.activityLength ?? 0;
 
         return (
-          <div key={index}>
+          <React.Fragment key={index}>
             <div className={viewType === "list" ? "activity-wrapper category-" + activity.category + " index-" + index + " list " + activity.alignment : "activity-wrapper category-" + activity.category}>
-            {/* We calculate the time the activity will render with this - activity.minutesFromDayStart * oneMinuteOfHeight + 60 * oneMinuteOfHeight + "%"
-            The time from day start times by "one minute of height" which is a calculation of what one minute of height will represent on the users screen
-            We then add one minute of height times by 60 which gives us an hour, this is so we have some spacing of one hour at the top which gives a cleaner feel */}
-            <div 
-              onClick={() => activityClickHandler(index)} 
-              className={`activity category-${activity.category} index-${index} ${activity.alignment}`} 
-              style={{ top: `${minutesFromDayStart * oneMinuteOfHeight + 60 * oneMinuteOfHeight}%`, height: `calc(${activityLength * oneMinuteOfHeight}% - 5px)` }}
-              role="button" 
-              tabIndex={0}
-            >
-              <div className="activity-container">
-                {viewType === "list" ?
-                  <>
-                    <h2>{activity.title}</h2>
-                    <h3>{startTimeFormatted} - {endTimeFormatted}<span className="bold"></span> - {activity.activityLength} minutes</h3>
-                    <p>{activity.details}</p>
-                  </> : 
-                  <>
-                    <h2><span className="bold">{activity.title}</span> - {activity.activityLength} minutes</h2>
-                    <p>{activity.details}</p>
-                  </>
-                }
+              {/* We calculate the time the activity will render with this - activity.minutesFromDayStart * oneMinuteOfHeight + 60 * oneMinuteOfHeight + "%"
+              The time from day start times by "one minute of height" which is a calculation of what one minute of height will represent on the users screen
+              We then add one minute of height times by 60 which gives us an hour, this is so we have some spacing of one hour at the top which gives a cleaner feel */}
+              <div 
+                onClick={() => activityClickHandler(index)} 
+                className={`activity category-${activity.category} index-${index} ${activity.alignment}`} 
+                style={{ top: `${minutesFromDayStart * oneMinuteOfHeight + 60 * oneMinuteOfHeight}%`, height: `calc(${activityLength * oneMinuteOfHeight}% - 5px)` }}
+                role="button" 
+                tabIndex={0}
+              >
+                <div className="activity-container">
+                  {viewType === "list" ?
+                    <>
+                      <h2>{activity.title}</h2>
+                      <h3>{startTimeFormatted} - {endTimeFormatted}<span className="bold"></span> - {activity.activityLength} minutes</h3>
+                      <p>{activity.details}</p>
+                    </> : 
+                    <>
+                      <h2><span className="bold">{activity.title}</span> - {activity.activityLength} minutes</h2>
+                      <p>{activity.details}</p>
+                    </>
+                  }
+                </div>
               </div>
             </div>
-          </div>
-          {activeModal === index && (
-            <Modal activity={activity} startTimeFormatted={startTimeFormatted} endTimeFormatted={endTimeFormatted} onClose={() => setActiveModal(null)} />
-          )}
-          </div>
+            {activeModal === index && (
+              <Modal activity={activity} startTimeFormatted={startTimeFormatted} endTimeFormatted={endTimeFormatted} onClose={() => setActiveModal(null)} />
+            )}
+          </React.Fragment>
         );
       })}
     </>
